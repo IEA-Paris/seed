5249ac8e2ca1aea1f37cabcfa64c9619f82e62d7
hbouare <hamed.bouare@paris-iea.fr>
2024-05-17 15:43:57 +0200

    feat: adding events css classes

--- pages/about/network.vue
+++ pages/about/network.vue
@@ -78,7 +78,7 @@
       </div>
 
       <div>
-        <v-row class="d-flex align-center justify-center main-container">
+        <v-row class="d-flex align-center justify-center">
           <v-col
             cols="12"
             md="2"
@@ -91,7 +91,7 @@
                 :src="item.picture"
                 :title="item.title"
                 fit:contain
-                class="fill-image"
+                class="fill-image-item"
               >
               </nuxt-img>
             </a>
@@ -105,6 +105,123 @@
               <ActionsSmallContainer
                 :action="action"
               ></ActionsSmallContainer> </v-sheet></v-col -->
+        </v-row>
+      </div>
+    </v-container>
+  </section>
+
+  <section>
+    <v-container> </v-container>
+  </section>
+  <section>
+    <v-container>
+      <div>
+        <v-row no-gutters>
+          <v-col cols="12" md="1">
+            <v-card class="mx-auto" variant="text">
+              <div>
+                <v-card class="mx-auto" variant="text">
+                  <v-card-item>
+                    <div class="card-title">30</div>
+                    <div class="card-title-subtitle">
+                      Apr <br />
+                      2024
+                    </div>
+                  </v-card-item>
+                </v-card>
+              </div>
+            </v-card></v-col
+          >
+
+          <v-col cols="12" md="5">
+            <v-card class="mx-auto" variant="text">
+              <v-card-item>
+                <div class="text-wrap">
+                  <span
+                    ><a
+                      href="https://www.radcliffe.harvard.edu/event/2024-alexis-pauline-gumbs-lecture"
+                      class="anchor-card-title"
+                    >
+                      Audre Lorde, June Jordan, and a Homemade Field of Love
+                    </a>
+                  </span>
+                </div>
+                <div class="text-wrap">
+                  <span class="anchor-lecture">
+                    Lectures •<a
+                      href="https://www.radcliffe.harvard.edu/events-and-exhibitions/series/kim-and-judy-davis-deans-lecture-in-the-humanities"
+                      title="Kim and Judy Davis Dean's Lecture in the Humanities"
+                      class="anchor-lecture"
+                    >
+                      Kim and Judy Davis Dean's Lecture in the Humanities</a
+                    ></span
+                  >
+                </div>
+              </v-card-item>
+              <v-card-text class="text-wrap content-item">
+                Alexis Pauline Gumbs will offer an ecofeminist exploration into
+                how the works of Black feminist poets Audre Lorde and June
+                Jordan can speak to our current climate crisis.
+              </v-card-text>
+
+              <div class="d-flex flex-row align-center justify-start">
+                <v-card-item>
+                  <div>
+                    <span>
+                      <v-icon color="#32c188" class="icon-fontsz"
+                        >mdi-circle</v-icon
+                      >
+                      <span class="ml-3 regi-item">Registration Open</span>
+                    </span>
+                  </div>
+                </v-card-item>
+
+                <v-card-item>
+                  <div>
+                    <span>
+                      <v-icon>mdi-television-play</v-icon>
+                      <span class="ml-3">Live stream available</span>
+                    </span>
+                  </div>
+                </v-card-item>
+              </div>
+            </v-card>
+          </v-col>
+
+          <v-col cols="12" md="3">
+            <div>
+              <v-card class="mx-auto" variant="text" color="#767676">
+                <v-card-item>
+                  <div>Date & Time</div>
+                  <div class="text-wrap content-item">
+                    Tuesday, April 30, 2024 4 PM ET
+                  </div>
+                </v-card-item>
+              </v-card>
+            </div>
+            <div>
+              <v-card class="mx-auto" variant="text" color="#767676">
+                <v-card-item>
+                  <div>Location</div>
+                  <div class="text-wrap content-item">
+                    Knafel Center OR Online on Zoom 10 Garden Street Cambridge,
+                    MA 02138
+                  </div>
+                </v-card-item>
+              </v-card>
+            </div>
+          </v-col>
+
+          <v-col cols="12" md="3">
+            <v-sheet class="mx-auto">
+              <nuxt-img
+                src="https://radcliffe-harvard-edu.imgix.net/7a3318d8-16f7-437e-b8c3-fbf9bbe1f926/Alexis-Pauline-Gumbs_by-Sufia-Ikbal-Doucet.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=0%2C0%2C1584%2C1584"
+                fit:cover
+                class="fill-image"
+              >
+              </nuxt-img>
+            </v-sheet>
+          </v-col>
         </v-row>
       </div>
     </v-container>
@@ -133,7 +250,8 @@ const logos = !logosData || !logosData.value ? undefined : logosData.value;
 </script>
 
 <style scoped>
-.fill-image {
+/* logo component */
+.fill-image-item {
   max-width: 100%;
   min-height: 100%;
   overflow: hidden;
@@ -141,9 +259,61 @@ const logos = !logosData || !logosData.value ? undefined : logosData.value;
   -webkit-filter: grayscale(100%);
   transition: filter 0.5s ease;
 }
-
-.fill-image:hover {
+/* logo component */
+.fill-image-item:hover {
   filter: none;
   -webkit-filter: grayscale(0);
+}
+
+.fill-image {
+  max-width: 100%;
+  min-height: 100%;
+}
+
+.card-title,
+.card-title-subtitle {
+  text-align: right;
+  color: #181818;
+  font-family: "Lelo", helvetica, arial, sans-serif;
+  font-weight: 400;
+}
+
+.card-title {
+  font-size: 24px;
+}
+
+.card-title-subtitle {
+  font-size: 16px;
+}
+.anchor-card-title {
+  text-decoration: none;
+  font-family: Rosart, serif;
+  font-weight: 400;
+  font-style: italic;
+  color: #181818;
+}
+.anchor-lecture {
+  color: #767676;
+  text-decoration: none;
+  font-family: Lelo, helvetica, arial, sans-serif;
+  font-weight: 500;
+  font-size: 0.875rem;
+  line-height: 130%;
+}
+
+.content-item {
+  color: #181818;
+  font-family: Lelo, helvetica, arial, sans-serif;
+}
+
+.card-align-item {
+  text-align: left;
+}
+
+.icon-fontsz {
+  font-size: 8px;
+}
+.regi-item {
+  color: #767676;
 }
 </style>
