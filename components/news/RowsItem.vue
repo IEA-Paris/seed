8e108fb9b48e66d60f28cea7f3391917bf5f7129
hbouare <hamed.bouare@paris-iea.fr>
2024-06-20 16:07:56 +0200

    feat: added first review comments

--- components/news/RowsItem.vue
+++ components/news/RowsItem.vue
@@ -1,8 +1,8 @@
 <template>
   {{ name }}
   <v-divider v-if="index > 0"></v-divider>
-  <v-row>
-    <v-col cols="12" md="3" lg="3" xl="3" v-if="mdAndUp">
+  <v-row class="mt-4">
+    <v-col cols="12" md="4" lg="3" xl="3" v-if="mdAndUp">
       <div class="overflow-hidden">
         <v-img
           :src="item.image"
@@ -14,69 +14,9 @@
       </div>
     </v-col>
 
-    <v-col cols="12" md="9" lg="9" xl="9" v-if="mdAndUp">
+    <v-col cols="12" md="8" lg="9" xl="9" v-if="mdAndUp">
       <v-row v-if="xl">
         <v-col cols="5">
-          <v-sheet>
-            <div>
-              <NuxtLink to="/" class="text-wrap text-h5 text-black">
-                {{ item.title }}</NuxtLink
-              >
-            </div>
-
-            <div class="d-flex flex-row flex-wrap">
-              <div v-for="tag in item.tags" :key="tag" class="mr-2 mt-2">
-                <v-tooltip location="bottom" :text="tag.name">
-                  <template v-slot:activator="{ props }">
-                    <v-chip variant="outlined" label v-bind="props">{{
-                      tag.name
-                    }}</v-chip>
-                  </template>
-                </v-tooltip>
-              </div>
-            </div>
-          </v-sheet>
-        </v-col>
-        <v-col cols="7">
-          <ContentRenderer :value="item" class="clamp-xl text-body-1" />
-          <v-btn variant="outlined">
-            {{ $t("learn-more") }}
-          </v-btn>
-        </v-col>
-      </v-row>
-
-      <v-row v-if="lg">
-        <v-col cols="5">
-          <v-sheet>
-            <div>
-              <NuxtLink to="/" class="text-wrap text-h5 text-black">
-                {{ item.title }}</NuxtLink
-              >
-            </div>
-
-            <div class="d-flex flex-row flex-wrap">
-              <div v-for="tag in item.tags" :key="tag" class="mr-2 mt-2">
-                <v-tooltip location="bottom" :text="tag.name">
-                  <template v-slot:activator="{ props }">
-                    <v-chip variant="outlined" label v-bind="props">{{
-                      tag.name
-                    }}</v-chip>
-                  </template>
-                </v-tooltip>
-              </div>
-            </div>
-          </v-sheet>
-        </v-col>
-        <v-col cols="7">
-          <ContentRenderer :value="item" class="clamp-lg text-body-1" />
-          <v-btn variant="outlined">
-            {{ $t("learn-more") }}
-          </v-btn>
-        </v-col>
-      </v-row>
-
-      <v-row v-if="md">
-        <v-col cols="12">
           <div>
             <NuxtLink to="/" class="text-wrap text-h5 text-black">
               {{ item.title }}</NuxtLink
@@ -85,13 +25,53 @@
 
           <div class="d-flex flex-row flex-wrap">
             <div v-for="tag in item.tags" :key="tag" class="mr-2 mt-2">
-              <v-tooltip location="bottom" :text="tag.name">
-                <template v-slot:activator="{ props }">
-                  <v-chip variant="outlined" label v-bind="props">{{
-                    tag.name
-                  }}</v-chip>
-                </template>
-              </v-tooltip>
+              <v-chip size="large" variant="outlined" label>{{
+                tag.name
+              }}</v-chip>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="7">
+          <ContentRenderer :value="item" class="clamp-xl text-body-1" />
+          <v-btn class="mt-4" variant="outlined">
+            {{ $t("learn-more") }}
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="lg">
+        <v-col cols="5">
+          <div>
+            <NuxtLink to="/" class="text-wrap text-h5 text-black">
+              {{ item.title }}</NuxtLink
+            >
+          </div>
+
+          <div class="d-flex flex-row flex-wrap">
+            <div v-for="tag in item.tags" :key="tag" class="mr-2 mt-2">
+              <v-chip variant="outlined" label>{{ tag.name }}</v-chip>
+            </div>
+          </div>
+        </v-col>
+        <v-col cols="7">
+          <ContentRenderer :value="item" class="clamp-lg text-body-1" />
+          <v-btn class="mt-8" variant="outlined">
+            {{ $t("learn-more") }}
+          </v-btn>
+        </v-col>
+      </v-row>
+
+      <v-row v-if="md">
+        <v-col cols="12">
+          <div>
+            <NuxtLink to="/" class="text-h5 text-black text-wrap">
+              {{ item.title }}</NuxtLink
+            >
+          </div>
+
+          <div class="d-flex flex-row flex-wrap">
+            <div v-for="tag in item.tags" :key="tag" class="mr-2 mt-2">
+              <v-chip variant="outlined" label>{{ tag.name }}</v-chip>
             </div>
           </div>
 
@@ -105,23 +85,8 @@
       </v-row>
     </v-col>
 
-    <v-col class="ml-4" cols="12" v-if="smAndDown">
-      <v-row>
-        <v-col cols="6">
-          <div class="overflow-hidden">
-            <v-img
-              :src="item.image"
-              :aspect-ratio="1 / 1"
-              cover
-              class="img-animation"
-            >
-            </v-img></div
-        ></v-col>
-        <v-col cols="6">
-          <MiscAtomsDateStamp :date="item.date" class="pr-6" />
-        </v-col>
-      </v-row>
-      <v-sheet class="mt-2">
+    <v-col cols="12" v-if="smAndDown">
+      <div class="ma-2 ma-sm-4">
         <div>
           <NuxtLink to="/" class="text-wrap text-h5 text-black">
             {{ item.title }}</NuxtLink
@@ -129,30 +94,35 @@
         </div>
 
         <div class="d-flex flex-row flex-wrap">
-          <div v-for="tag in item.tags" :key="tag" class="mr-2 mt-2">
-            <v-tooltip location="bottom" :text="tag.name">
-              <template v-slot:activator="{ props }">
-                <v-chip variant="outlined" label v-bind="props">{{
-                  tag.name
-                }}</v-chip>
-              </template>
-            </v-tooltip>
+          <div
+            v-for="tag in item.tags"
+            :key="tag"
+            class="mr-2 mt-2 mr-sm-3 mt-sm-4"
+          >
+            <v-chip size="small" variant="outlined" label>{{
+              tag.name
+            }}</v-chip>
           </div>
         </div>
-      </v-sheet>
 
-      <ContentRenderer :value="item" class="clamp-sm-and-down text-body-1" />
-      <v-btn variant="outlined" class="mt-2">
-        {{ $t("learn-more") }}
-      </v-btn>
+        <ContentRenderer
+          :value="item"
+          class="clamp-sm-and-down text-body-1 mt-2 mt-sm-3"
+        />
+
+        <div class="d-flex flex-column mt-2 mt-sm-4">
+          <v-btn variant="outlined">
+            {{ $t("learn-more") }}
+          </v-btn>
+        </div>
+      </div>
     </v-col>
   </v-row>
 </template>
 
 <script setup>
 import { useDisplay } from "vuetify"
-const { name, sm, md, xl, lg, smAndUp, smAndDown, mdAndUp, lgAndUp } =
-  useDisplay()
+const { name, md, xl, lg, smAndDown, mdAndUp } = useDisplay()
 const props = defineProps({
   item: {
     type: Object,
@@ -184,7 +154,7 @@ console.log("titi", props.item)
 }
 
 .clamp-md {
-  @include clamp(2);
+  @include clamp(4);
 }
 
 .clamp-sm-and-down {
