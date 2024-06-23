01afa3f87b53565db23b95a772c78870088be53a
hbouare <hamed.bouare@paris-iea.fr>
2024-06-23 22:57:43 +0200

    feat: adding news view structure

---
+++ components/news/View.vue
@@ -0,0 +1,19 @@
+<template>
+  {{ name }}
+  <v-row>
+    <v-col cols="12"> {{ item.title }}</v-col>
+  </v-row>
+</template>
+
+<script setup>
+import { useDisplay } from "vuetify"
+const { name, lgAndUp, mdAndUp, smAndDown, sm, xs } = useDisplay()
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped></style>
