3011f3902fb7ba979d2e713d8d9f2999391f732a
hbouare <hamed.bouare@paris-iea.fr>
2024-07-24 22:38:04 +0200

    feat: added rowItem skeleton loader

--- components/people/RowsItem.vue
+++ components/people/RowsItem.vue
@@ -3,46 +3,72 @@
   <v-divider v-if="index > 0"></v-divider>
   <v-row class="my-6 ml-md-1">
     <v-col cols="12" md="3" v-if="mdAndUp">
-      <div class="overflow-hidden">
-        <v-img
-          :src="item.image"
-          :aspect-ratio="1 / 1"
-          cover
-          class="img-animation"
-        >
-        </v-img>
-      </div>
+      <v-skeleton-loader
+        v-if="rootStore.loading"
+        height="100%"
+        type="image"
+      ></v-skeleton-loader>
+      <template v-else>
+        <div class="overflow-hidden">
+          <v-img
+            :src="item.image"
+            :aspect-ratio="1 / 1"
+            cover
+            class="img-animation"
+          >
+          </v-img>
+        </div>
+      </template>
     </v-col>
 
     <v-col cols="12" md="8">
-      <div class="ml-md-8">
-        <NuxtLink to="/" class="text-wrap text-h4 text-black">
-          {{ item.title }}</NuxtLink
-        >
-        <div class="ml-n3 my-2">
-          <PeopleIconBadge :socials="item.socials" />
+      <v-skeleton-loader
+        v-if="rootStore.loading"
+        :type="
+          [
+            'heading, subtitle, text@5',
+            'heading, subtitle, text@5',
+            'heading, subtitle, text@3',
+            'heading, subtitle, text@7',
+            'heading, subtitle, text@11',
+            'heading, subtitle, text@11',
+          ][['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')]
+        "
+      ></v-skeleton-loader>
+
+      <template v-else>
+        <div class="ml-md-8">
+          <NuxtLink to="/" class="text-wrap text-h4 text-black">
+            {{ item.title }}</NuxtLink
+          >
+          <div class="ml-n3 my-2">
+            <PeopleIconBadge :socials="item.socials" />
+          </div>
+          <ContentRenderer
+            :value="item"
+            class="text-body-1 clamped-text mt-n3"
+            :style="
+              '-webkit-line-clamp:' +
+              [5, 5, 3, 5, 9, 9][
+                ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')
+              ]
+            "
+          />
         </div>
-        <ContentRenderer
-          :value="item"
-          class="text-body-1 clamped-text mt-n3"
-          :style="
-            '-webkit-line-clamp:' +
-            [5, 5, 4, 5, 9, 9][
-              ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')
-            ]
-          "
-        />
-      </div>
+      </template>
     </v-col>
     <v-col cols="12" md="1"></v-col>
   </v-row>
 </template>
 <script setup>
+import { useRootStore } from "~/store/root"
+
 import { highlight } from "~/assets/utils/transforms"
 import { useDisplay } from "vuetify"
 const { name, mdAndDown, md, xl, lg, smAndDown, mdAndUp, lgAndUp } =
   useDisplay()
 
+const rootStore = useRootStore()
 const props = defineProps({
   item: {
     type: Object,
