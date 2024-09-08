344240249875f8f4199cd0e13724cc9341afeaed
hbouare <hamed.bouare@paris-iea.fr>
2024-09-08 13:11:54 +0200

    feat: debugging project rowitem

--- components/project/RowsItem.vue
+++ components/project/RowsItem.vue
@@ -2,29 +2,33 @@
   <!--  TODO: design properly -->
   <v-col cols="12" sm="6" md="4">
     <div v-if="rootStore.loading || rootStore.project.loading">LOADING</div>
-    <v-card
-      v-else
-      class="d-flex flex-row pa-6 align-start"
-      :to="
-        localePath(getSlugFromPath(item._path))
-        /*'/activities/projects/' +
+
+    <v-card v-else class="d-flex flex-row pa-6 align-start">
+      <nuxt-link
+        :to="
+          localePath({
+            name: 'project-slug',
+            params: { slug: getSlugFromPath(item._path) },
+          })
+          /*'/activities/projects/' +
           item._file.split('/').pop().slice(0, -3) /* TODO : make it cleaner */
-      "
-    >
-      <MiscAtomsImageContainer
-        :src="item.picture"
-        class="align-start"
-        :loading="rootStore.project.loading"
-        :ratio="1 / 1"
-        :title="item.title"
-        :link="
-          localePath({ name: 'project-slug', params: { slug: item.slug } })
         "
       >
-      </MiscAtomsImageContainer>
-      <div>
-        <v-card-title>{{ item.title }}</v-card-title>
-      </div>
+        <MiscAtomsImageContainer
+          :src="item.picture"
+          class="align-start"
+          :loading="rootStore.project.loading"
+          :ratio="1 / 1"
+          :title="item.title"
+          :link="
+            localePath({ name: 'project-slug', params: { slug: item._path } })
+          "
+        >
+        </MiscAtomsImageContainer>
+        <div>
+          <v-card-title>{{ item.title }}</v-card-title>
+        </div>
+      </nuxt-link>
     </v-card>
   </v-col>
 </template>
--- components/project/View.vue
+++ components/project/View.vue
@@ -1,9 +1,141 @@
 <template>
-  <div>
-    <!--  TODO: create from the slug page and bind it to it -->
-  </div>
+  <!--  {{ name }} -->
+  <v-row>
+    <v-col
+      cols="12"
+      v-if="smAndDown"
+      class="text-wrap text-h4 text-black mx-sm-6"
+    >
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        :type="['heading', 'heading'][['xs', 'sm'].indexOf(name || 'sm')]"
+      ></v-skeleton-loader>
+      <template v-else>
+        {{ item.title }}
+      </template>
+    </v-col>
+    <v-col cols="12" md="4" class="pb-0">
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        height="100%"
+        type="image"
+      ></v-skeleton-loader>
+
+      <div v-else class="mx-sm-6">
+        <MiscAtomsImageContainer
+          :loading="rootStore.news.loading"
+          :src="item.image"
+          :ratio="1 / 1"
+        />
+      </div>
+    </v-col>
+
+    <v-col cols="12" md="8" class="pl-0 pb-0">
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        :type="
+          [
+            'avatar, paragraph',
+            'avatar, paragraph',
+            'heading, ossein,	avatar, text, ossein, chip@3',
+            'heading, ossein,	avatar, text, ossein, chip@3',
+            'heading, ossein, avatar, text, ossein, chip@3',
+            'heading',
+          ][['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')]
+        "
+      ></v-skeleton-loader>
+
+      <template v-else>
+        <div class="mx-4 mx-md-0">
+          <div class="d-flex text-wrap text-h4 text-black" v-if="mdAndUp">
+            {{ item.title }}
+          </div>
+          <div
+            class="d-flex flex-column flex-md-row align-md-center mt-6 mx-sm-6"
+          >
+            <div class="ml-md-8">
+              <div class="mt-lg-n1">
+                <MiscMoleculesChipContainer
+                  :items="item.tags"
+                ></MiscMoleculesChipContainer>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </v-col>
+  </v-row>
+
+  <v-row class="mt-12">
+    <v-col cols="12" md="4" :order="mdAndUp ? 'first' : 'last'" class="pt-0">
+      <MiscMoleculesRelatedItems
+        type="events"
+        :items="item.relatedEvents"
+        class="mx-6 my-2"
+      ></MiscMoleculesRelatedItems>
+      <MiscMoleculesRelatedItems
+        type="news"
+        :items="item.relatedNews"
+        class="mx-6 my-2"
+      ></MiscMoleculesRelatedItems>
+      <MiscMoleculesRelatedItems
+        type="people"
+        :items="item.relatedPeople"
+        class="mx-6 my-2"
+      ></MiscMoleculesRelatedItems>
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        :type="
+          [
+            'image, text@3, ossein, button',
+            'image, text@3, ossein, button',
+            'image, text@3, ossein, button',
+            'image, text@3, ossein, button',
+          ][['md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')]
+        "
+      ></v-skeleton-loader>
+      <template v-else>
+        <div class="mt-2 mx-n6 mx-sm-0" v-if="mdAndUp">
+          <ActionsSmallContainer
+            :action="action"
+            :ratio="1"
+          ></ActionsSmallContainer>
+        </div>
+      </template>
+    </v-col>
+    <v-col cols="12" md="8" class="pl-0 pt-0">
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        :type="
+          ['text@50', 'text@50', 'text@50', 'text@50', 'text@50', 'text@50'][
+            ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')
+          ]
+        "
+      ></v-skeleton-loader>
+      <template v-else>
+        <ContentRenderer :value="item" class="mt-md-n2 mx-10 mx-md-0" />
+      </template>
+    </v-col>
+  </v-row>
 </template>
 
-<script setup></script>
+<script setup>
+import { useDisplay } from "vuetify"
+const { name, mdAndUp, smAndDown } = useDisplay()
+const router = useRouter()
+const { locale } = useI18n()
+import { useRootStore } from "~/store/root"
+const rootStore = useRootStore()
+const props = defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+})
 
-<style lang="scss" scoped></style>
+const { data: action } = await useAsyncData("actions", () =>
+  queryContent("/actions/" + locale.value)
+    .limit(1)
+    .find(),
+)
+</script>
