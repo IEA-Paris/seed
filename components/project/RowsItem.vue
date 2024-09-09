2e5173f49bee1f3b291d97446c5b85f41729f4b4
CI <username@users.noreply.github.com>
2024-09-09 10:02:42 +0200

    fix: project rowItem

--- components/project/RowsItem.vue
+++ components/project/RowsItem.vue
@@ -1,35 +1,22 @@
 <template>
   <!--  TODO: design properly -->
-  <v-col cols="12" sm="6" md="4">
+  <v-col cols="12" sm="6" md="3">
     <div v-if="rootStore.loading || rootStore.project.loading">LOADING</div>
-
-    <v-card v-else class="d-flex flex-row pa-6 align-start">
-      <nuxt-link
-        :to="
-          localePath({
-            name: 'project-slug',
-            params: { slug: getSlugFromPath(item._path) },
-          })
-          /*'/activities/projects/' +
-          item._file.split('/').pop().slice(0, -3) /* TODO : make it cleaner */
-        "
+    <div v-else>
+      <MiscAtomsImageContainer
+        :src="item.picture"
+        class="align-start"
+        :loading="rootStore.project.loading"
+        :ratio="1 / 1"
+        :title="item.title"
+        link="activities-projects-slug"
+        :slug="getSlugFromPath(item._path)"
       >
-        <MiscAtomsImageContainer
-          :src="item.picture"
-          class="align-start"
-          :loading="rootStore.project.loading"
-          :ratio="1 / 1"
-          :title="item.title"
-          :link="
-            localePath({ name: 'project-slug', params: { slug: item._path } })
-          "
-        >
-        </MiscAtomsImageContainer>
-        <div>
-          <v-card-title>{{ item.title }}</v-card-title>
-        </div>
-      </nuxt-link>
-    </v-card>
+      </MiscAtomsImageContainer>
+      <div class="text-h5 pt-3">
+        {{ item.title }}
+      </div>
+    </div>
   </v-col>
 </template>
 
