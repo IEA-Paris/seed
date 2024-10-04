e06751b654fe2766d3d470c0d3adab7e4a521d07
hbouare <165926077+hbouare@users.noreply.github.com>
2024-10-04 16:15:26 +0200

    Feat.added tag (#61)
    * fix new view squeleton loader
    
    * feat: added news dense squeleton loader
    
    * feat:added dense people view
    
    * feat: make people rowitembody and image clickable
    
    * make event and people bodies clickable
    
    * fix: searchInput tile attribute
    
    * fix: rowItem links
    
    * fix: rowItem links
    
    * fix: rowItem links
    
    ---------
    
    Co-authored-by: CI <username@users.noreply.github.com>

--- components/news/DenseItem.vue
+++ components/news/DenseItem.vue
@@ -4,26 +4,45 @@
     class="cursor-pointer"
     @click="$router.push(localePath('/news/' + getSlugFromPath(item._path)))"
   >
-    <v-col align-self="center" cols="2" class="text-h6 dense" v-if="mdAndUp">
-      <v-chip class="mb-4">{{ $t("news.categories." + item.category) }}</v-chip>
+    <v-col align-self="center" cols="7" class="text-h6 dense">
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        type="heading"
+      ></v-skeleton-loader>
+      <template v-else>
+        {{ item.title }}
+      </template>
     </v-col>
-    <v-col align-self="center" cols="6" class="text-h6 dense">
-      {{ item.title }}
+
+    <v-col align-self="center" cols="5" class="dense">
+      <v-skeleton-loader
+        v-if="rootStore.loading || rootStore.news.loading"
+        :type="
+          ['chip', 'chip@2', 'chip@3', 'chip@4', 'chip@4', 'chip@4'][
+            ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')
+          ]
+        "
+      ></v-skeleton-loader>
+
+      <template v-else>
+        <MiscMoleculesChipContainer
+          :items="item.tags"
+          size="small"
+        ></MiscMoleculesChipContainer>
+      </template>
     </v-col>
-    <v-col align-self="center" cols="4" lg="4" class="dense">
-      <MiscMoleculesChipContainer
-        :items="item.tags"
-        size="small"
-      ></MiscMoleculesChipContainer
-    ></v-col>
   </v-row>
 </template>
 <script setup>
 import { useDisplay } from "vuetify"
 import { useRootStore } from "~/store/root"
 
+<<<<<<< HEAD
+const { name } = useDisplay()
+=======
 const { locale } = useI18n()
 const { name, smAndUp, mdAndDown, mdAndUp, lgAndUp } = useDisplay()
+>>>>>>> main
 const localePath = useLocalePath()
 const rootStore = useRootStore()
 const props = defineProps({
