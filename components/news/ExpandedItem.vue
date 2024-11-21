d3f1ae87805e6438ca7d2f72d6a1992c559a28d8
Billybobbonnet <boitalettre9@gmail.com>
2024-11-21 10:38:47 +0100

    wip: expanded item

--- components/news/ExpandedItem.vue
+++ components/news/ExpandedItem.vue
@@ -1,66 +1,59 @@
 <template>
-  <v-expansion-panels class="mb-4">
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <v-row>
-          <v-col class="text-h5" cols="12" md="7">
-            {{ item.title }}
-          </v-col>
+  <div
+    class="expanded-item"
+    :class="expanded ? 'expanded' : 'collapsed'"
+    @click="expanded = !expanded"
+  >
+    <v-row>
+      <v-col cols="12" :md="expanded ? '4' : '2'">
+        <MiscAtomsImageContainer
+          cover
+          :loading="rootStore.loading"
+          :src="item.image"
+          :ratio="1 / 1"
+        />
+      </v-col>
+      <v-col class="text-h5" cols="12" :md="expanded ? '8' : '10'">
+        {{ item.title }}
+        <ContentRenderer
+          :value="item"
+          class="text-body-1 clamped-text"
+          :style="
+            '-webkit-line-clamp:' +
+            [5, 5, 4, 7, 8, 10][
+              ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')
+            ]
+          "
+        />
+        <template v-if="expanded">
+          <MiscMoleculesChipContainer
+            :items="item.tags"
+          ></MiscMoleculesChipContainer>
 
-          <v-col cols="12" md="5">
-            <MiscMoleculesChipContainer
-              :items="item.tags"
-            ></MiscMoleculesChipContainer>
-          </v-col>
-        </v-row>
-      </v-expansion-panel-title>
-
-      <v-expansion-panel-text>
-        <v-row>
-          <v-col cols="12" md="2">
-            <MiscAtomsImageContainer
-              cover
-              :loading="rootStore.loading"
-              :src="item.image"
-              :ratio="1 / 1"
-            />
-          </v-col>
-          <v-col cols="12" md="9">
-            <ContentRenderer
-              :value="item"
-              class="text-body-1 clamped-text"
-              :style="
-                '-webkit-line-clamp:' +
-                [5, 5, 4, 7, 8, 10][
-                  ['xs', 'sm', 'md', 'lg', 'xl', 'xxl'].indexOf(name || 'md')
-                ]
-              "
-            />
-            <v-btn
-              class="mt-4"
-              variant="outlined"
-              tile
-              size="small"
-              :to="
-                localePath({
-                  name: 'news-slug',
-                  params: { slug: getSlugFromPath(item._path) },
-                })
-              "
-            >
-              {{ $t("read-more") }}
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
+          <v-btn
+            class="mt-4"
+            variant="outlined"
+            tile
+            size="small"
+            :to="
+              localePath({
+                name: 'news-slug',
+                params: { slug: getSlugFromPath(item._path) },
+              })
+            "
+          >
+            {{ $t("read-more") }}
+          </v-btn></template
+        >
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script setup>
 import { useRootStore } from "~/store/root"
 const rootStore = useRootStore()
-
+const expanded = ref(false)
 const props = defineProps({
   item: {
     type: Object,
@@ -73,4 +66,17 @@ const props = defineProps({
 })
 </script>
 
-<style></style>
+<style lang="scss">
+.expanded-item {
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+}
+
+.expanded-item.expanded {
+  height: auto;
+}
+.expanded-item.collapsed {
+  height: 100px;
+  overflow: hidden;
+}
+</style>
