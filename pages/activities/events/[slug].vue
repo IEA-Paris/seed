306a9120c4c569e241eaae6eee54d46fbd963eee
Billybobbonnet <boitalettre9@gmail.com>
2025-04-02 13:40:42 +0200

    feat: new getItem pattern

--- pages/activities/events/[slug].vue
+++ pages/activities/events/[slug].vue
@@ -5,8 +5,16 @@
 </template>
 
 <script setup>
-import { useContentItem } from "~/composables/useContentItem"
 import GET_EVENT from "~/graphql/queries/item/events.gql"
 
-const { data: event, loading } = useContentItem(GET_EVENT, "getEvent", "events")
+const { fetchItem } = useFetchItem()
+const loading = ref(true)
+// Fetch the item
+const { data: event } = await useAsyncData("item", async () => {
+  return await fetchItem({
+    query: GET_EVENT,
+    key: "getEvent",
+  })
+})
+loading.value = false
 </script>
