31cc56933140d1fed6bebcd8dcf0e0b67d07ed75
Billybobbonnet <boitalettre9@gmail.com>
2025-06-26 09:26:46 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -113,7 +113,8 @@ export default defineNuxtConfig({
     "@nuxt/test-utils/module",
     /*     "@nuxtjs/html-validator", */
     "@stefanobartoletti/nuxt-social-share",
-    "./modules/list/src/module",
+    /* "./modules/list/src/module", // To use to debug list */
+    "@paris-ias/list",
   ],
   list: {
     modules: [
