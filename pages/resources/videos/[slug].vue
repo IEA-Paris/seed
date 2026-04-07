ffb50474419d8a97f345674254d885a26394ce0d
billybobbonnet <boitalettre9@gmail.com>
2026-04-07 16:50:50 +0200

    feat: new pages matching the new navigation

---
+++ pages/resources/videos/[slug].vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container> <NewsView :item="news" :loading /></v-container>
+</template>
+
+<script setup>
+const { $queries } = useNuxtApp()
+const { resourceItem: news, loading } = useI18nResourceItem({
+  resourceName: "news",
+  documentGql: $queries.news.get,
+  responseKey: "getNews",
+  appId: "iea",
+})
+</script>
---
+++ pages/resources/videos/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <div class="d-flex justify-center">
+          <h2 class="text-center">{{ capitalize($t("items.videos", 2)) }}</h2>
+        </div>
+        <ListOrganismsList
+          type="news"
+          path-prefix="resources.videos-slug"
+        ></ListOrganismsList> </v-col
+    ></v-row>
+  </v-container>
+</template>
+
+<script setup></script>
