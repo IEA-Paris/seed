519a05af4afac886ca48a49c2563dee8429e970b
billybobbonnet <boitalettre9@gmail.com>
2026-06-04 14:43:23 +0200

    chore: bumped list package version, renamed collective intelligence sections

---
+++ pages/programs/collective-intelligence/projects/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <MiscMoleculesSection
+    :title="capitalize($t('items.projects', 2))"
+    markdown-file="programs__collective-intelligence__projects__projects"
+    type="projects"
+    path-prefix="programs-collective-intelligence-projects-slug"
+    modifier="projects"
+  />
+</template>
